0a6b9d9268f96944c7eaae18a0ebd6f341111a7b
andresmelek <candres.isaza@gmail.com>
2020-04-29 12:44:40 -0500

    State objects that handles all default RestFul API actions

---
+++ api/v1/views/states.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+File to work with Amenities
+"""
+
+from models.base_model import *
+from api.v1.views import app_views
+from models import storage
+from models.state import *
+from flask import jsonify, abort, request, make_response
+
+
+@app_views.route('/states', strict_slashes=False, methods=['GET', 'POST'])
+def states():
+    """Function that retrieve and save a new State"""
+    ls = []
+    states = storage.all('State').values()
+    if request.method == "GET":
+        for state in states:
+            ls.append(state.to_dict())
+        return jsonify(ls)
+    elif request.method == "POST":
+        if not request.json:
+            return make_response(jsonify({'error': "Not a JSON"}), 400)
+        elif 'name' not in request.json:
+            return make_response(jsonify({'error': "Missing name"}), 400)
+        else:
+            new = State(**request.json)
+            new.save()
+            return make_response(new.to_dict(), 201)
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False,
+                 methods=['GET', 'DELETE', 'PUT'])
+def state(state_id):
+    """Function that retrieve, delete and put a State"""
+    state = storage.get(State, state_id)
+    if state:
+        if request.method == "GET":
+            return state.to_dict()
+        elif request.method == "DELETE":
+            storage.delete(state)
+            storage.save()
+            return {}
+        elif request.method == "PUT":
+            if not request.json:
+                return make_response(jsonify({'error': "Not a JSON"}), 400)
+            else:
+                json = request.json
+                for key, value in json.items():
+                    if key != 'id' and key != 'created_at' and\
+                       key != "updated_at":
+                        setattr(state, key, value)
+                state.updated_at = datetime.utcnow()
+                storage.save()
+                return make_response(state.to_dict(), 200)
+    abort(404)
